58ca015c721006a5e5e95fbd2b77907ccdd1bfad
Capybara16-20 <capybara20.07@mail.ru>
2020-01-28 01:19:35 +0300

    Lesson-9/Class "Snake"

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -21,6 +21,9 @@ namespace Snake
             leftLine.Drow();
             rightLine.Drow();
 
+            Point p1 = new Point(4, 5, '*');
+            Snake snake = new Snake(p1, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadKey();
         }
     }
