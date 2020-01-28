98d3bed5b9378fef349fbf885b67dc34414e77b4
Capybara16-20 <capybara20.07@mail.ru>
2020-01-28 03:08:01 +0300

    Lesson-11/Control snake

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -59,5 +59,10 @@ namespace Snake
             sym = ' ';
             Draw();
         }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -25,15 +25,30 @@ namespace Snake
             Point p1 = new Point(4, 5, '*');
             Snake snake = new Snake(p1, 4, Direction.RIGHT);
             snake.Drow();
-            
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true)
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(300);
+
+                Thread.Sleep(100);
                 snake.Move();
             }
 
